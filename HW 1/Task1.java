1307933c541b7eb92e11f7c17486833efc1311a0
yuratereshko <tereshko1994@gmail.com>
2018-05-14 23:38:53 +0300

    hw

--- HW 1/Task1.java
+++ HW 1/Task1.java
@@ -1,15 +1,14 @@
-
+import java.util.Scanner;
 public class Task1 {
+   static Scanner sc = new Scanner(System.in);
+   public static void main(String args[])
+   {
+      System.out.print("Radius:");
 
-	public static void main(String[] args) {
-		int radius = 5;
-		double perimeter = Math.PI * (radius * radius);
-		double area =  2 * radius * Math.PI;
-		
-	
-	System.out.println("Perimeter: " + perimeter);
-	System.out.println("Area: " + area);
-		
-	}
-
+      double radius = sc.nextDouble();
+      double area = Math.PI * (radius * radius);
+      System.out.println("Area: " + area);
+      double perimeter= Math.PI * 2*radius;
+      System.out.println("Perimeter: " + perimeter);
+   }
 }
--- HW 1/Task2.java
+++ HW 1/Task2.java
@@ -1,12 +1,25 @@
 
-public class Task2 {
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	
 
-	public static void main(String[] args) {
-		String name = "Yura";
-		String adress = "Lviv";
+public class Task2 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader (
+				new InputStreamReader(System.in));
 		
-		System.out.println("What is your name? "  + name);
-		System.out.println("Where are you live, Yura?" + ' ' + adress);
+		
+		System.out.println("What is your name?");
+		String name = br.readLine();
+		System.out.println("Where do you live?");
+		String adress = br.readLine();
+
+		System.out.println("You are " + name + " and you live in " + adress);  
+		
+
+
 
 	}
 
