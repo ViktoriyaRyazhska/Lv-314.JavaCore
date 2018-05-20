c7d2592bc00007b35e45acd46d6a510126051917
Petr <kirkland@i.ua>
2018-05-20 21:05:17 +0300

    test

--- HW/L4/HW/part2/TEST/DogTest.java
+++ HW/L4/HW/part2/TEST/DogTest.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterEach;1
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
