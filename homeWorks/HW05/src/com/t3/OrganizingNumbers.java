e98926c128a652523c398782e9a5beee22b981a0
Dmytro Malakhov <dmytro.malakhov@outlook.com>
2018-05-29 23:55:52 +0300

    added method organizing numbers

--- homeWorks/HW05/src/com/t3/OrganizingNumbers.java
+++ homeWorks/HW05/src/com/t3/OrganizingNumbers.java
@@ -1,7 +1,21 @@
 package com.t3;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.homework.Main.br;
+
 public class OrganizingNumbers {
-    public static void organizingNumbers() {
-        System.out.println("without solving");
+    public static void organizingNumbers() throws IOException {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(0,1);
+        for (int i = 0; i < 100; i++) {
+            if (numbers.get(i) > 0) {
+                numbers.add(Integer.parseInt( br.readLine() ) );
+            } else {
+                System.out.println("Last integer < 0");
+                System.out.println(numbers);
+            }
+        }
     }
 }
