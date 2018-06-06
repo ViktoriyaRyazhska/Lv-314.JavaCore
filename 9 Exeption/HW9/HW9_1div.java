b944e58b94614bfee6507e07b2c6dc30536a1979
TarasL <l.taras88@gmail.com>
2018-06-06 15:37:38 +0300

    add HW9.1

---
+++ 9 Exeption/HW9/HW9_1div.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HW9_1div {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		double a = 1;
+		double b = 1;
+		try {
+			a = Double.parseDouble(br.readLine());
+			b = Double.parseDouble(br.readLine());
+			System.out.println(div(a, b));
+		} catch (NumberFormatException e) {
+			System.err.println("input only digits");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("some problem with console");
+			e.printStackTrace();
+		} catch (ArithmeticException e) {
+			System.err.println("/0");
+		}
+	}
+
+	public static double div(double a, double b) throws ArithmeticException {
+		if (b == 0) {
+			throw new ArithmeticException("/0");
+		}
+		return a / b;
+	}
+
+}
