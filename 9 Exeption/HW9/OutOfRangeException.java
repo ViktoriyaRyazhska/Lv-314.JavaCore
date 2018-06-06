77f4c0afa259d6a7a7bd4e3747695e3ca6ae6f15
TarasL <l.taras88@gmail.com>
2018-06-06 16:29:20 +0300

    add HW9.2

---
+++ 9 Exeption/HW9/OutOfRangeException.java
@@ -0,0 +1,11 @@
+
+public class OutOfRangeException extends Exception {
+private static final long serialVersionUID = 1L;
+	
+	public OutOfRangeException(String msg) {
+		super(msg);
+	}
+	public OutOfRangeException () {
+
+	}
+}
