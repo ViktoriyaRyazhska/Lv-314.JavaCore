0035574ba0987ae1f91c492859251fd8ddea5760
TarasL <l.taras88@gmail.com>
2018-06-06 16:35:16 +0300

    add HW9

--- 9 Exeption/HW9/HW9_2.java
+++ 9 Exeption/HW9/HW9_2.java
@@ -1,33 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HW9_2 {
 
 	public static void main(String[] args) {
+
+		int start = 1;
+		int end = 100;
+		int i = 1;
+		List<Double> arr = new ArrayList<Double>();
+		do {
+			System.out.println("Input digit #" + i + " in range [" + start + ";" + end + "]");
+			try {
+				arr.add(readNumber(start, end));
+				i++;
+			} catch (NumberFormatException e) {
+				System.err.println("not a digit");
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			} catch (OutOfRangeException e) {
+				System.err.println(e.getMessage());
+			}
+		} while (arr.size() < 10);
+		
+		
+		System.out.println(arr);
+	}
+
+	public static double readNumber(int start, int end) throws NumberFormatException, IOException, OutOfRangeException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		double a = 1;
-		double b = 1;
-		try {
-			a = Double.parseDouble(br.readLine());
-			b = Double.parseDouble(br.readLine());
-			System.out.println(div(a, b));
-		} catch (NumberFormatException e) {
-			System.err.println("input only digits");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("some problem with console");
-			e.printStackTrace();
-		} catch (ArithmeticException e) {
-			System.err.println("/0");
-		}
+		double a = Double.parseDouble(br.readLine());
+		if (a <= start || a >= end) {
+			throw new OutOfRangeException("Out of range");
+		} else
+			return a;
 	}
-
-	public static double div(double a, double b) throws ArithmeticException {
-		if (b == 0) {
-			throw new ArithmeticException("/0");
-		}
-		return a / b;
-	}
-
 }
