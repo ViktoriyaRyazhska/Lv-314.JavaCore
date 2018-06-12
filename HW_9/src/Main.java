6d9540bad20517e3a064556cf3000e814361f75b
proonk <proonk@gmail.com>
2018-06-12 15:23:15 +0300

    HW_9_upd

--- HW_9/src/Main.java
+++ HW_9/src/Main.java
@@ -11,6 +11,9 @@ public class Main {
 		try {
 			System.out
 					.println("Result is: " + div(Double.parseDouble(rd.readLine()), Double.parseDouble(rd.readLine())));
+		} catch (INumberException e) {
+			System.err.print(e.getMessage());
+			;
 		} catch (NumberFormatException e) {
 			System.err.print("Entered value is not a number");
 		} catch (Exception e) {
@@ -19,26 +22,28 @@ public class Main {
 
 		try {
 			readNumber(0, 10);
+
+		} catch (NumberFormatException | IOException e) {
+			System.out.println("Entered value is not a number or Input/Output problem occured");
 		} catch (INumberException e) {
 			System.err.println(e.getMessage());
 		}
 
 	}
 
-	public static double div(double a, double b) {
+	public static double div(double a, double b) throws INumberException {
+		if (b == 0) {
+			throw new INumberException("Second argument cannot be zero");
+		}
 		return a / b;
 	}
 
-	public static void readNumber(int start, int end) throws INumberException {
-		try {
-			int a = Integer.parseInt(rd.readLine());
-			if (a > start & a < end) {
-				System.out.println(a);
-			} else {
-				throw new INumberException("Entered number does not satisfy the condition");
-			}
-		} catch (NumberFormatException | IOException e) {
-			System.out.println("Entered value is not a number or Input/Output problem occured");
+	public static void readNumber(int start, int end) throws INumberException, NumberFormatException, IOException {
+		int a = Integer.parseInt(rd.readLine());
+		if (a > start & a < end) {
+			System.out.println(a);
+		} else {
+			throw new INumberException("Entered number does not satisfy the condition");
 		}
 
 	}
